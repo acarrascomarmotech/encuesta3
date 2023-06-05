904ee009003779547deb70af858a75ac99f456e3
acarrascomarmotech <acarrasco@marmotech.com.do>
2023-06-04 22:47:58 -0400

    another

--- pages/api/encrypt.js
+++ pages/api/encrypt.js
@@ -1,11 +1,15 @@
 import jwt from 'jsonwebtoken';
+import {desencriptarParametro} from "../../utils/webtoken"
 
 const secretKey = "marmotech";
 
 export default function encryptParameter(req, res) {
   // Crea el token JWT utilizando el parámetro y la clave secreta
-  const parametro = req.body.params
+  const parametro = req.body
   const token = jwt.sign({ parametro }, secretKey);
+
+  const parametroDesencriptado = desencriptarParametro(token)
+  console.log(parametroDesencriptado);
 
 	return res.status(200).json({ token });
 }
